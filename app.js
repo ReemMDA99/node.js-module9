2481af093d01384ff6639d0384b3e83a7180ce9f
Reem Ahmed <reem.mda0909@gmail.com>
2022-04-11 17:23:34 -0600

    refractor

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
-// console.log('Hello Node!');
-// console.log('document');
-// var message ='Hello Node!';
-// var sum = 5 + 3;
-//  console.log(message);
-// //  console.log(sum);
-// var commandLineArgs = process.argv;
-// console.log(commandLineArgs);
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => 
-      console.log(profileItem));
-  };
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
