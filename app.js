5a3e274634894db83059ddc4b5da6482edce6a90
Reem Ahmed <reem.mda0909@gmail.com>
2022-04-12 09:46:20 -0600

    generate html

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const profileDataArgs = process.argv.slice(2);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
 
-  console.log('================');
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err =>{
+  if (err) throw new Error(err);
+  console.log('Portfolio complete! Checkout index.html to see the output!');
+});
